f2116e5272e38751bde59707c4f24a5f8c298f60
ClaytonSiby <claytonsiby@gmail.com>
2023-08-03 22:57:40 +0200

    Refactor: display methods sequence

--- src/main.py
+++ src/main.py
@@ -17,7 +17,9 @@ class Main:
         dragger = self.game.dragger
 
         while True:
+            # show methods
             game.show_bg(screen)
+            game.show_moves(screen)
             game.show_pieces(screen)
 
             if dragger.is_dragging:
@@ -31,13 +33,20 @@ class Main:
 
                     if board.squares[clicked_row][clicked_col].has_piece():
                         piece = board.squares[clicked_row][clicked_col].piece
+                        board.calculate_moves(piece, clicked_row, clicked_col)
                         dragger.save_initial_position(event.pos)
                         dragger.drag_piece(piece)
+                        # show methods
+                        game.show_bg(screen)
+                        game.show_moves(screen)
+                        game.show_pieces(screen)
 
                 elif event.type == pygame.MOUSEMOTION:
                     if dragger.is_dragging:
                         dragger.update_mouse(event.pos)
+                        # show methods
                         game.show_bg(screen)
+                        game.show_moves(screen)
                         game.show_pieces(screen)
                         dragger.update_blit(screen)
                 
