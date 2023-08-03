3747ad72aa35d6bd89bd879c3cc89a56bc260a96
ClaytonSiby <claytonsiby@gmail.com>
2023-08-03 22:56:19 +0200

    Add: method to calculate possbile moves for the knight

--- src/board.py
+++ src/board.py
@@ -1,6 +1,7 @@
 from const import *
 from square import Square
 from piece import *
+from move import *
 
 class Board:
     def __init__(self):
@@ -39,3 +40,53 @@ class Board:
 
         # king
         self.squares[row_other][4] = Square(row_other, 4, King(color))
+
+    def calculate_moves(self, piece, row, col):
+        '''
+          Calculate all the possible valid moves of a specific piece on a specific position
+        '''
+
+        def knight_moves():
+            # 8 possible moves for the knight on the center of the board
+            possible_moves = [
+                (row - 2, col + 1),
+                (row - 1, col + 2),
+                (row + 1, col + 2),
+                (row + 2, col + 1),
+                (row + 2, col - 1),
+                (row + 1, col - 2),
+                (row - 1, col - 2),
+                (row - 2, col - 1)
+            ]
+
+            for possible_move in possible_moves:
+                possible_move_row, possible_move_col = possible_move
+                if Square.in_range(possible_move_row, possible_move_col):
+                    if self.squares[possible_move_row][possible_move_col].is_empty_or_has_rival_piece(piece.color):
+                        # create squares of the new move
+                        initial_square = Square(row, col)
+                        final_square   = Square(possible_move_row, possible_move_col) # piece = piece
+
+                        # create new move
+                        move = Move(initial_square, final_square)
+
+                        # add move
+                        piece.add_move(move)
+
+        if isinstance(piece, Pawn):
+            pass
+
+        elif isinstance(piece, Knight):
+            knight_moves()
+
+        elif isinstance(piece, Bishop):
+            pass
+
+        elif isinstance(piece, Rook):
+            pass
+
+        elif isinstance(piece, Queen):
+            pass
+
+        elif isinstance(piece, King):
+            pass
