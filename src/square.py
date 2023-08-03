6db7ebf9e1db8a05a8fa6532b336ef1453a3840a
ClaytonSiby <claytonsiby@gmail.com>
2023-08-03 22:59:05 +0200

    Add: some Square instance methods

--- src/square.py
+++ src/square.py
@@ -6,3 +6,23 @@ class Square:
 
     def has_piece(self):
         return self.piece != None
+
+    def is_empty(self):
+        return not self.has_piece()
+    
+    def has_team_piece(self, color):
+        return self.has_piece() and self.piece.color == color
+    
+    def has_rival_piece(self, color):
+        return self.has_piece() and self.piece.color != color
+
+    def is_empty_or_has_rival_piece(self, color):
+        return self.is_empty() or self.has_rival_piece(color)
+    
+    @staticmethod
+    def in_range(*args):
+        for arg in args:
+            if arg< 0 or arg > 7:
+                return False
+            
+        return True
