aa97239480c123d1f505fd939723be9496d5b0d7
Clayton Siby <claytonsiby@gmail.com>
2023-09-07 13:14:03 +0200

    Add: method to perform equality comparisons on objects

--- src/move.py
+++ src/move.py
@@ -3,3 +3,10 @@ class Move:
         self.initial_square   = initial_square
         self.final_square = final_square
 
+    def __str__(self):
+        s = ''
+        s += f'({self.initial_squarecol}, {self.final_square.row})'
+        s += f' -> ({self.initial_square.col}, {self.final_square.row})'
+
+    def __eq__(self, other):
+        return self.initial_square == other.initial_square and self.final_square == other.final_square
